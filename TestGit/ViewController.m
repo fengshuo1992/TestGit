f351739b9f195360030c0cf4d20bbead595edecb
冯硕 <fengshuo@51jnq.com>
2018-01-22 11:12:01 +0800

    提交

---
+++ TestGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGit
+//
+//  Created by 杭州米发 on 2018/1/22.
+//  Copyright © 2018年 冯硕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by 杭州米发 on 2018/1/22.
+//  Copyright © 2018年 冯硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
