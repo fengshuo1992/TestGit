26e6e5f8d59cbe729721dc197618cfa9e7dbda73
冯硕 <fengshuo@51jnq.com>
2018-02-08 14:19:37 +0800

    增加了name累

---
+++ TestGit/Name.h
@@ -0,0 +1,13 @@
+//
+//  Name.h
+//  TestGit
+//
+//  Created by 杭州米发 on 2018/2/8.
+//  Copyright © 2018年 冯硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Name : UIView
+
+@end
---
+++ TestGit/Name.m
@@ -0,0 +1,21 @@
+//
+//  Name.m
+//  TestGit
+//
+//  Created by 杭州米发 on 2018/2/8.
+//  Copyright © 2018年 冯硕. All rights reserved.
+//
+
+#import "Name.h"
+
+@implementation Name
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
